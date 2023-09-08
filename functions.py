40fbc1ed7ae3691f37e7fb15e8c27ee52e27dfce
jegerud <kristjeg@stud.ntnu.no>
2023-09-08 16:03:50 +0200

    Added wavelet t

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fft import fft, fftfreq
 from tftb.processing import WignerVilleDistribution, inst_freq, plotifl
-from scipy.signal import hamming
+from scipy.signal import hamming, cwt, ricker
 from tftb.processing import Spectrogram
 
 N = 3000
@@ -53,3 +53,12 @@ def wvt(signal):
 def ht(signal):
     ifr = inst_freq(signal)[0]
     plotifl(np.linspace(0,len(ifr), len(ifr)), ifr)
+
+# Wavelet Transform
+def wt(signal):
+    widths = np.arange(1, 31)
+    cwtmatr = cwt(signal, ricker, widths)
+    cwtmatr_yflip = np.flipud(cwtmatr)
+    plt.imshow(cwtmatr_yflip, extent=[-1, 1, 1, 31], cmap='PRGn', aspect='auto',
+            vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
+    plt.show()
