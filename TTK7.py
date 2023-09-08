3d254cadb359481f39b49a396f8b940dc646483b
Sunniva Freyer <sunniva@freyer.no>
2023-09-08 16:04:04 +0200

    h

--- TTK7.py
+++ TTK7.py
@@ -5,6 +5,7 @@ from scipy.fft import fft, fftfreq
 from tftb.processing import WignerVilleDistribution, inst_freq, plotifl
 from scipy.signal import hamming
 from tftb.processing import Spectrogram
+from functions import *
 
 
 
@@ -36,78 +37,43 @@ signal = np.sum(components, axis=0)
 # plt.grid(True)
 # plt.show()
 
-
-## Analyse the final signal: stationary or non-stationary
-# The final signal is stationary because it doesn't change over time
-
-# Run a FFT analysis to get an idea of the frequency components. 
-# Reflect on the results of this analysis
-def fft(signal):
-    fft_result = np.fft.fft(signal)
-    frequencies = np.fft.fftfreq(len(fft_result), 1 / fs)
-
-    plt.figure(figsize=(8, 4))
-    plt.plot(frequencies, np.abs(fft_result))
-    plt.title("FFT of the Signal")
-    plt.xlabel("Frequency (Hz)")
-    plt.ylabel("Amplitude")
-    plt.grid(True)
-    plt.xlim(-50, 50)
-    plt.show()
-
-## How to decide the window size if STFT or WT is going to be used?
-# Since we are dealing with a stationary signal a longer window is preferred to capture
-# more frequency details. A longer window is also preferred as the frequency components are of low frequency.
-
-
-## Which signal processing technique is best for your signal (FFT, STFT, WVT, WT, HT)?
-
-# STFT
-def stft(signal):
-    fwindow = hamming(1000)
-    spec = Spectrogram(signal, n_fbins=128, fwindow=fwindow)
-    spec.run()
-    spec.plot(kind="contour", threshold=0.1, show_tf=False)
-
-# WVT
-def wvt(signal): 
-    n_points = 128
-    fmin, fmax = 0.0, 0.5
-    wvd = WignerVilleDistribution(signal)
-    wvd.run()
-    wvd.plot(kind='contour', extent=[0, n_points, fmin, fmax])
-
-
-# Hilbert Transform / Instantaneous Frequency
-def ht(signal):
-    ifr = inst_freq(signal)[0]
-    plotifl(np.linspace(0,len(ifr), len(ifr)), ifr)
+fft(signal)
 
 
 
-# # Add an offset and repeat the analysis
-# offset = 2
-# signal_o = signal + offset
+# Add an offset and repeat the analysis
+offset = 2
+signal_o = signal + offset
 
-# plt.figure(figsize=(10,4))
-# plt.plot(t, signal_o)
-# plt.title('Signal with offset')
-# plt.xlabel('Time (s)')
-# plt.ylabel('Amplitude')
-# plt.grid(True)
-# plt.show()
+plt.figure(figsize=(10,4))
+plt.plot(t, signal_o)
+plt.title('Signal with offset')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
+plt.show()
 
-# # Add white noise and repeat the analysis
-# mean = 0
-# std = 0.5
-# num_samples = len(t)
-# white_noise = np.random.normal(mean, std, size=num_samples)
+fft(signal_o)
+stft(signal)
+wvt(signal_o)
+ht(signal_o)
 
-# signal_wn = signal + white_noise
-# plt.figure(figsize=(10,4))
-# plt.plot(t, signal_wn)
-# plt.title('Signal with white noise')
-# plt.show()
+# Add white noise and repeat the analysis
+mean = 0
+std = 0.5
+num_samples = len(t)
+white_noise = np.random.normal(mean, std, size=num_samples)
+
+signal_wn = signal + white_noise
+plt.figure(figsize=(10,4))
+plt.plot(t, signal_wn)
+plt.title('Signal with white noise')
+plt.show()
+
+fft(signal_wn)
+stft(signal_wn)
+wvt(signal_wn)
+ht(signal_wn)
 
 # # Add a linearly time varying frequency component and repeat the analysis (frequency=kt)
 # fmin, fmax = 0.0, 0.05
