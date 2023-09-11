efa61e861956f1b7b077bb0ec900089d0039e5f7
jegerud <kristjeg@stud.ntnu.no>
2023-09-11 15:26:59 +0200

    Added functionality to plot the transforms for different signals

--- TTK7.py
+++ TTK7.py
@@ -34,84 +34,99 @@ plt.ylabel('Amplitude')
 plt.grid(True)
 plt.show()
 
-fft(signal)
-stft(signal)
-wvt(signal)
-wt(signal)
-ht(signal)
+# freq, fft_result = fft(signal)
+# f, t, Sxx = stft(signal)
+# wvd, n_points, fmin, fmax = wvt(signal)
+# cwtmatr = wt(signal)
+# analytic_signal, amplitude_envelope, instantaneous_frequency = ht(signal)
 
 # Add an offset and repeat the analysis
 offset = 2
 signal_o = signal + offset
 
-plt.figure(figsize=(10,4))
-plt.plot(t, signal_o)
-plt.title('Signal with offset')
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-plt.grid(True)
-plt.show()
+# plt.figure(figsize=(10,4))
+# plt.plot(t, signal_o)
+# plt.title('Signal with offset')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Amplitude')
+# plt.grid(True)
+# plt.show()
 
-fft(signal_o)
-stft(signal_o)
-wvt(signal_o)
-wt(signal_o)
-ht(signal_o)
+# fft(signal_o)
+# stft(signal_o)
+# wvt(signal_o)
+# wt(signal_o)
+# ht(signal_o)
 
 # Add white noise and repeat the analysis
 mean = 0
 std = 3
-num_samples = len(t)
-white_noise = np.random.normal(mean, std, size=num_samples)
+white_noise = np.random.normal(mean, std, size=(num_samples,))
 
 signal_wn = signal + white_noise
-plt.figure(figsize=(10,4))
-plt.plot(t, signal_wn)
-plt.title('Signal with white noise')
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-plt.grid(True)
-plt.show()
+# plt.figure(figsize=(10,4))
+# plt.plot(t, signal_wn)
+# plt.title('Signal with white noise')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Amplitude')
+# plt.grid(True)
+# plt.show()
 
-fft(signal_wn)
-stft(signal_wn)
-wvt(signal_wn)
-wt(signal_wn)
-ht(signal_wn)
+# fft(signal_wn)
+# stft(signal_wn)
+# wvt(signal_wn)
+# wt(signal_wn)
+# ht(signal_wn)
 
 # Add a linearly time varying frequency component and repeat the analysis (frequency=kt)
 fmin, fmax = 0.0, 0.05
 
-lin_var_freq_comp, _ = fmlin(len(t), fmin, fmax)
+lin_var_freq_comp, _ = fmlin(num_samples, fmin, fmax)
 lin_var_freq_comp = np.real(lin_var_freq_comp)
 signal_lfq = signal + lin_var_freq_comp
 
-plt.figure(figsize=(10,4))
-plt.plot(t, signal_lfq)
-plt.title('Signal with Linearly varying frequency component')
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-plt.grid(True)
-plt.show()
+# plt.figure(figsize=(10,4))
+# plt.plot(t, signal_lfq)
+# plt.title('Signal with Linearly varying frequency component')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Amplitude')
+# plt.grid(True)
+# plt.show()
 
-fft(signal_lfq)
-stft(signal_lfq)
-wvt(signal_lfq)
-wt(signal_lfq)
-ht(signal_lfq)
+# fft(signal_lfq)
+# stft(signal_lfq)
+# wvt(signal_lfq)
+# wt(signal_lfq)
+# ht(signal_lfq)
 
 # Add an offset and white noise and repeat the analysis
 signal_wn_and_o = signal + white_noise + offset
-plt.figure(figsize=(10, 4))
-plt.plot(t, signal_wn_and_o)
-plt.title('Signal with offset and white noise')
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-plt.grid(True)
-plt.show()
+# plt.figure(figsize=(10, 4))
+# plt.plot(t, signal_wn_and_o)
+# plt.title('Signal with offset and white noise')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Amplitude')
+# plt.grid(True)
+# plt.show()
 
-fft(signal_wn_and_o)
-stft(signal_wn_and_o)
-wvt(signal_wn_and_o)
-wt(signal_wn_and_o)
-ht(signal_wn_and_o)
+# fft(signal_wn_and_o)
+# stft(signal_wn_and_o)
+# wvt(signal_wn_and_o)
+# wt(signal_wn_and_o)
+# ht(signal_wn_and_o)
+
+labels = ['Original', 'Offset', 'White Noise', 'Linearly Time Varying Frequency', 'White Noise and Offset']
+signals = [signal, signal_o, signal_wn, signal_lfq, signal_wn_and_o]
+plot_originals(signals, labels)
+
+ffts = [fft(signal), fft(signal_o), fft(signal_wn), fft(signal_lfq), fft(signal_wn_and_o)]
+plot_ffts(ffts, labels)
+
+stfts = [stft(signal), stft(signal_o), stft(signal_wn), stft(signal_lfq), stft(signal_wn_and_o)]
+plot_stfts(stfts, labels)
+
+hts = [ht(signal), ht(signal_o), ht(signal_wn), ht(signal_lfq), ht(signal_wn_and_o)]
+plot_hts(signal, hts, labels)
+
+wts = [wt(signal), wt(signal_o), wt(signal_wn), wt(signal_lfq), wt(signal_wn_and_o)]
+plot_wts(wts, labels)
--- functions.py
+++ functions.py
@@ -13,6 +13,7 @@ num_samples = int(sample_rate * duration)
 
 # Create a time vector
 time = np.linspace(0, duration, num_samples, endpoint=False)
+plot_default = False
 
 # Run a FFT analysis to get an idea of the frequency components. 
 # Reflect on the results of this analysis
@@ -20,15 +21,17 @@ def fft(signal):
     fft_result = np.fft.fft(signal)
     freq = np.fft.fftfreq(num_samples, 1 / sample_rate)
 
-    # Plot the magnitude spectrum
-    plt.figure(figsize=(10, 6))
-    plt.plot(freq, np.abs(fft_result))
-    plt.title('FFT Analysis')
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Magnitude')
-    plt.grid(True)
-    plt.xlim(-20, 20)  # Limit the x-axis to show frequencies up to 20 Hz
-    plt.show()
+    if plot_default:
+        # Plot the magnitude spectrum
+        plt.figure(figsize=(10, 6))
+        plt.plot(freq, np.abs(fft_result))
+        plt.title('FFT Analysis')
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Magnitude')
+        plt.grid(True)
+        plt.xlim(-20, 20)  # Limit the x-axis to show frequencies up to 20 Hz
+        plt.show()
+    return (freq, np.abs(fft_result))
 
 
 ## Which signal processing technique is best for your signal (FFT, STFT, WVT, WT, HT)?
@@ -36,20 +39,23 @@ def fft(signal):
 # STFT
 def stft(signal):
     f, t, Sxx = spectrogram(signal, fs=sample_rate, nperseg=1000)
-    plt.pcolormesh(t, f, np.abs(Sxx), shading='gouraud')
-    plt.title('STFT Magnitude')
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-    plt.show()
+    if plot_default:
+        plt.pcolormesh(t, f, np.abs(Sxx), shading='gouraud')
+        plt.title('STFT Magnitude')
+        plt.ylabel('Frequency [Hz]')
+        plt.xlabel('Time [sec]')
+        plt.show()
+    return f, t, np.abs(Sxx)
 
 # WVT
 def wvt(signal): 
     n_points = 128
     fmin, fmax = 0.0, 0.5
     wvd = WignerVilleDistribution(signal)
-    wvd.run()
-    wvd.plot(kind='contour', extent=[0, n_points, fmin, fmax])
-
+    if plot_default:
+        wvd.run()
+        wvd.plot(kind='contour', extent=[0, n_points, fmin, fmax])
+    return wvd, n_points, fmin, fmax
 
 # Hilbert Transform / Instantaneous Frequency
 def ht(signal):
@@ -58,38 +64,154 @@ def ht(signal):
     instantaneous_phase = np.unwrap(np.angle(analytic_signal))
     instantaneous_frequency = (np.diff(instantaneous_phase) / (2.0*np.pi) * sample_rate)
     
-    plt.figure(figsize=(12, 6))
-    plt.title('Hilbert Transform')
+    if plot_default:
+        plt.figure(figsize=(12, 6))
+        plt.title('Hilbert Transform')
 
-    plt.subplot(211)
-    plt.plot(time, signal, label='Original Signal')
-    plt.plot(time, amplitude_envelope, label='Instantaneous Amplitude')
-    plt.title('Original Signal')
-    plt.xlabel('Time (s)')
-    plt.grid(True)
+        plt.subplot(211)
+        plt.plot(time, signal, label='Original Signal')
+        plt.plot(time, amplitude_envelope, label='Instantaneous Amplitude')
+        plt.title('Original Signal')
+        plt.xlabel('Time (s)')
+        plt.grid(True)
 
-    plt.subplot(212)
-    plt.plot(time[1:], instantaneous_frequency , label='Instantaneous Frequency')
-    plt.title('Instantaneous Frequency')
-    plt.xlabel('Time (s)')
-    plt.grid(True)
+        plt.subplot(212)
+        plt.plot(time[1:], instantaneous_frequency , label='Instantaneous Frequency')
+        plt.title('Instantaneous Frequency')
+        plt.xlabel('Time (s)')
+        plt.grid(True)
 
-    plt.tight_layout()
-    plt.show()
+        plt.tight_layout()
+        plt.show()
+    return analytic_signal, amplitude_envelope, instantaneous_frequency
+
 
 # Wavelet Transform
 def wt(signal):
     widths = np.arange(1, 31)
     cwtmatr = cwt(signal, ricker, widths)
     cwtmatr_yflip = np.flipud(cwtmatr)
+    if (plot_default):
+        # Plot the CWT matrix
+        plt.imshow(cwtmatr_yflip, extent=[-1, 1, 1, 31], cmap='PRGn', aspect='auto',
+                vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
+        
+        # Add axis labels
+        plt.xlabel('Time')
+        plt.ylabel('Scale')
+        plt.title('Wavelet Transform')
+        
+        plt.show()
+    return cwtmatr
+
+def plot_originals(signals, labels):
+    plt.figure(figsize=(8, 2*len(labels)))
+    for idx, (signal, label) in enumerate(zip(signals, labels)):
+        plt.subplot(len(labels), 1, idx+1)
+        plt.plot(time, signal)
+        plt.title(label)
+        if (idx == len(labels)-1):
+            plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Amplitude')
+        ax = plt.gca()
+        ax.set_xticklabels([])
+        plt.grid(True)
     
-    # Plot the CWT matrix
-    plt.imshow(cwtmatr_yflip, extent=[-1, 1, 1, 31], cmap='PRGn', aspect='auto',
-               vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
+    plt.subplots_adjust(top=0.962, bottom=0.055, left=0.10, right=0.95, hspace=0.20,
+                    wspace=0.219)
+    plt.show()
+
+def plot_ffts(ffts, labels):
+    plt.figure(figsize=(8, 2*len(labels)))
+    # plt.suptitle('FFT Analysis')
+    for idx, (fft, label) in enumerate(zip(ffts, labels)):
+        freq, fft_result = fft
+        plt.subplot(len(labels), 1, idx+1)
+        plt.plot(freq, fft_result)
+        plt.title(label)
+        if (idx == len(labels)-1):
+            plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Magnitude')
+        ax = plt.gca()
+        ax.set_xticklabels([])
+        plt.grid(True)
+        plt.xlim(-20, 20)  # Limit the x-axis to show frequencies up to 20 Hz
     
-    # Add axis labels
-    plt.xlabel('Time')
-    plt.ylabel('Scale')
-    plt.title('Wavelet Transform')
+    plt.subplots_adjust(top=0.962, bottom=0.055, left=0.10, right=0.95, hspace=0.20,
+                    wspace=0.219)
+    plt.show()
+
+def plot_stfts(stfts, labels):
+    plt.figure(figsize=(8, 2*len(labels)))
     
+    for idx, (stft, label) in enumerate(zip(stfts, labels)):
+        f, t, Sxx = stft
+        plt.subplot(len(labels), 1, idx+1)
+        plt.pcolormesh(t, f, np.abs(Sxx), shading='gouraud')
+        plt.title(label)
+        plt.ylabel('Frequency [Hz]')
+        if (idx == len(labels)-1):
+            plt.xlabel('Time [sec]')
+        ax = plt.gca()
+        ax.set_xticklabels([])
+        plt.grid(True)
+        plt.ylim(0, 40)
+    
+    plt.subplots_adjust(top=0.962, bottom=0.055, left=0.10, right=0.95, hspace=0.20,
+                    wspace=0.219)
+    plt.show()
+
+def plot_hts(signal, ht, labels):
+    plt.figure(figsize=(14, 2*len(labels)))
+    # plt.suptitle('FFT Analysis')
+    for idx, (ht, label) in enumerate(zip(ht, labels)):
+        analytic_signal, amplitude_envelope, instantaneous_frequency= ht
+        plt.subplot(len(labels), 2, (2*idx+1))
+        
+        plt.plot(time, signal, label='Original Signal')
+        plt.plot(time, amplitude_envelope, label='Instantaneous Amplitude')
+        plt.title(label)
+        ax = plt.gca()
+        ax.set_xticklabels([])
+        plt.grid(True)
+        if (idx == len(labels)-1):
+            plt.xlabel('Time [sec]')
+
+        plt.subplot(len(labels), 2, (2*idx+1)+1)
+        plt.plot(time[1:], instantaneous_frequency , label='Instantaneous Frequency')
+        plt.title(label)
+        ax = plt.gca()
+        ax.set_xticklabels([])
+        plt.grid(True)
+        if (idx == len(labels)-1):
+            plt.xlabel('Time [sec]')
+        plt.tight_layout()
+    
+    plt.subplots_adjust(top=0.962, bottom=0.055, left=0.10, right=0.95, hspace=0.20,
+                    wspace=0.219)
+    plt.show()
+
+def plot_wts(wts, labels):
+    plt.figure(figsize=(8, 2*len(labels)))
+    
+    for idx, (wt, label) in enumerate(zip(wts, labels)):
+        cwtmatr = wt
+        cwtmatr_yflip = np.flipud(cwtmatr)
+
+        plt.subplot(len(labels), 1, idx+1)
+        plt.imshow(cwtmatr_yflip, extent=[-1, 1, 1, 31], cmap='PRGn', aspect='auto',
+                vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
+        
+        
+        plt.ylabel('Scale')
+        plt.title(label)
+        if (idx == len(labels)-1):
+            plt.xlabel('Time')
+        ax = plt.gca()
+        ax.set_xticklabels([])
+        # plt.grid(True)
+        # plt.ylim(0, 40)
+    
+    plt.subplots_adjust(top=0.962, bottom=0.055, left=0.10, right=0.95, hspace=0.20,
+                    wspace=0.219)
     plt.show()
